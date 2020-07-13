7a2f711f8da7e159836d34519665e80d2bfcfe39
izielin <misiopar@wp.pl>
2020-07-13 10:51:58 +0200

    adding resource bundles

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -13,12 +16,14 @@ public class Main extends Application {
     }
 
     public void start(Stage primaryStage) throws Exception {
-
+        Locale.setDefault(new Locale("en"));
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("/fxml/BorderPaneMain.fxml"));
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
         BorderPane borderPane = loader.load();
         Scene scene = new Scene(borderPane);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("My library");
+        primaryStage.setTitle(bundle.getString("tittle.application"));
         primaryStage.show();
     }
 }
